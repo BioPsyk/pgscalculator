55a5d2decb449e770036a58eccc351bd945f5c4a
pappewaio <j.r.gadin@gmail.com>
2024-11-29 11:12:34 +0100

    the actual fix for this issue

--- bin/make_augmented_output.sh
+++ bin/make_augmented_output.sh
@@ -24,59 +24,59 @@ awk -v FS="\t" -v OFS="\t" \
  -v posterior="$posterior" \
  -v benchmark="$benchmark" \
 '
-    BEGIN {
-
-        # Read mapfile into map array (ss_SNP to bim_SNP)
-        while ((getline < mapfile) > 0) map[$3] = $6;
-
-        # Read maffile
-        FS=" ";
-        while ((getline < maffile) > 0) mafHash[$2] = $5;
-        FS="\t";
-
-        # Read posterior and benchmark files into their respective arrays
-        while ((getline < posterior) > 0) posteriorHash[$4] = $3;
-        while ((getline < benchmark) > 0) benchmarkHash[$4] = $3;
-        
-        # Determine the index of each target column
-        n = split(colnames, cols, " ");
-        for (i=1; i<=n; i++){
+  BEGIN {
+      # Read mapfile into map array (ss_SNP to bim_SNP)
+      while ((getline < mapfile) > 0) map[$3] = $6;
+  
+      # Read maffile
+      FS=" ";
+      while ((getline < maffile) > 0) mafHash[$2] = $5;
+      FS="\t";
+  
+      # Read posterior and benchmark files into their respective arrays
+      while ((getline < posterior) > 0) posteriorHash[$4] = $3;
+      while ((getline < benchmark) > 0) benchmarkHash[$4] = $3;
+      
+      # Split column names and initialize indices
+      n = split(colnames, cols, " ");
+      for (i=1; i<=n; i++){
           colIdx[cols[i]] = -1;
-          printf "%s%s", cols[i], OFS ;
-        }
-        print "genoID" OFS "MAF" OFS "postEffect" OFS "OtherEffect"
-        for (i=1; i<=NF; i++) {
+      }
+  
+      # Print headers once
+      for (i=1; i<=n; i++){
+          printf "%s%s", cols[i], (i<n ? OFS : "");
+      }
+      print OFS "genoID" OFS "MAF" OFS "postEffect" OFS "benchEffect"
+  
+      # Process input file header once
+      getline
+      for (i=1; i<=NF; i++) {
           if ($i in colIdx){
-            colIdx[$i] = i;
+              colIdx[$i] = i;
           }
-        }
-        getline
-        for (i=1; i<=NF; i++) {
-          if ($i in colIdx){
-            colIdx[$i] = i;
-          }
-        }
-    }
-    FNR > 1 { 
-        # Assuming RSID will always be in column 2
-        rsid = $4; 
-        if (rsid in map) {
-            genoID = map[rsid];
-        } else {
-            print "ssID not in map: " rsid > "/dev/stderr";
-            exit 1;
-        }
+      }
+  }
+  FNR > 1 { 
+      # Assuming RSID will always be in column 2
+      rsid = $4; 
+      if (rsid in map) {
+          genoID = map[rsid];
+      } else {
+          print "ssID not in map: " rsid > "/dev/stderr";
+          exit 1;
+      }
 
-        postEffect = (genoID in posteriorHash) ? posteriorHash[genoID] : "NA";
-        benchEffect = (genoID in benchmarkHash) ? benchmarkHash[genoID] : "NA";
-        mafVal = (genoID in mafHash) ? mafHash[genoID] : "NA";
-        
-        # Print the selected columns for this row
-        for (i=1; i<=n; i++) {
-            idx = colIdx[cols[i]];
-            printf "%s%s", (idx > 0 ? $idx : "NA"), (i<n ? OFS : "");
-        }
-        print OFS genoID OFS mafVal OFS postEffect OFS benchEffect; # Append the new fields
-    }
+      postEffect = (genoID in posteriorHash) ? posteriorHash[genoID] : "NA";
+      benchEffect = (genoID in benchmarkHash) ? benchmarkHash[genoID] : "NA";
+      mafVal = (genoID in mafHash) ? mafHash[genoID] : "NA";
+      
+      # Print the selected columns for this row
+      for (i=1; i<=n; i++) {
+          idx = colIdx[cols[i]];
+          printf "%s%s", (idx > 0 ? $idx : "NA"), (i<n ? OFS : "");
+      }
+      print OFS genoID OFS mafVal OFS postEffect OFS benchEffect; # Append the new fields
+  }
 ' "$sumstat" 
 
